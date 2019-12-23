f0242233d0e49830a0cfe8fc4dd2b778898a3387
Oskar Janusz <janusz.oskar@gmail.com>
2019-12-23 16:49:44 +0100

    Allow overriding methods defined by the gem

--- lib/active_record/events.rb
+++ lib/active_record/events.rb
@@ -11,22 +11,6 @@ module ActiveRecord
     def has_event(name, options = {})
       naming = Naming.new(name, options)
 
-      define_method("#{naming.predicate}?") do
-        self[naming.field].present?
-      end
-
-      define_method("#{naming.inverse_predicate}?") do
-        self[naming.field].blank?
-      end
-
-      define_method(naming.action) do
-        touch(naming.field) if self[naming.field].blank?
-      end
-
-      define_method("#{naming.action}!") do
-        touch(naming.field)
-      end
-
       define_singleton_method(naming.collective_action) do
         update_all(naming.field => Time.current)
       end
@@ -38,6 +22,26 @@ module ActiveRecord
       define_singleton_method(naming.inverse_scope) do
         where(arel_table[naming.field].eq(nil))
       end
+
+      mod = Module.new
+      include mod
+      mod.class_eval <<-RUBY, __FILE__, __LINE__+1
+        define_method("#{naming.predicate}?") do
+          self[naming.field].present?
+        end
+  
+        define_method("#{naming.inverse_predicate}?") do
+          self[naming.field].blank?
+        end
+  
+        define_method(naming.action) do
+          touch(naming.field) if self[naming.field].blank?
+        end
+  
+        define_method("#{naming.action}!") do
+          touch(naming.field)
+        end
+      RUBY
     end
   end
 end
