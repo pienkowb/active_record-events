6fe2485330ee107c3860794489543dc29ad1aad7
Bartosz Pieńkowski <pienkowb@gmail.com>
2019-12-23 21:30:25 +0100

    Fix backward compatibility

--- lib/active_record/events/naming.rb
+++ lib/active_record/events/naming.rb
@@ -6,7 +6,7 @@ module ActiveRecord
       def initialize(infinitive, options = {})
         @infinitive = infinitive
         @object = options[:object].presence
-        @field_type = options[:field_type]&.to_sym
+        @field_type = options[:field_type].try(:to_sym)
       end
 
       def field
