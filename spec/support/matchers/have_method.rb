2b9de7fab26b9ac63b996afd963d966b923168ec
Bartosz Pieńkowski <pienkowb@gmail.com>
2026-01-31 14:24:26 +0100

    Use the `public_method_defined?` method

--- spec/support/matchers/have_method.rb
+++ spec/support/matchers/have_method.rb
@@ -1,5 +1,5 @@
 RSpec::Matchers.define :have_method do |expected|
   match do |actual|
-    actual.public_instance_methods.include?(expected)
+    actual.public_method_defined?(expected)
   end
 end
